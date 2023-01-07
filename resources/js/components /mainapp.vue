d502f7f0f9d7a765eacaeb48495baa2df31b5248
Lameck Meshack <rhinolamer@gmail.com>
2023-01-08 00:16:16 +0300

    Finished vue configuration and loaded first component

---
+++ resources/js/components /mainapp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>This is the first component</h1>
+    </div>
+</template>
+<style>
+   
+</style>
+
+<script>
+
+    export default{
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel  FullStack Blog</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body>
-        <h1>TRial for the body</h1>
-    </body>
-    
+    <title>Laravel FullStack Blog</title>
+
+</head>
+
+<body>
+    <div id="app">
+
+        <mainapp></mainapp>
+    </div>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+
 </html>
