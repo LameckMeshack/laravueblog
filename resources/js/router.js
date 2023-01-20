f0215c31abf509976bc8f692923907519339d0e4
Lameck Meshack <rhinolamer@gmail.com>
2023-01-20 14:26:26 +0300

    Added login route

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,6 +4,7 @@ import home from "./components/pages/home.vue";
 import tags from "./admin/pages/tags.vue";
 import category from "./admin/pages/category.vue";
 import adminusers from "./admin/pages/adminusers.vue";
+import login from "./admin/pages/login.vue";
 Vue.use(Router);
 
 const routes = [
@@ -11,6 +12,7 @@ const routes = [
     { path: "/tags", component: tags },
     { path: "/category", component: category },
     { path: "/admin", component: adminusers },
+    { path: "/login", component: login },
 ];
 
 export default new Router({
