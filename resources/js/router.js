d477b9a00bfa85ea4b0c7349688e3933e2ad288c
Lameck Meshack <rhinolamer@gmail.com>
2023-01-23 14:35:06 +0300

    Routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,6 +5,7 @@ import tags from "./admin/pages/tags.vue";
 import category from "./admin/pages/category.vue";
 import adminusers from "./admin/pages/adminusers.vue";
 import login from "./admin/pages/login.vue";
+import role from "./admin/pages/role.vue";
 Vue.use(Router);
 
 const routes = [
@@ -13,6 +14,7 @@ const routes = [
     { path: "/category", component: category },
     { path: "/admin", component: adminusers },
     { path: "/login", component: login },
+    { path: "/role", component: role },
 ];
 
 export default new Router({
