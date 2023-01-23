14faee31be49011615bd4866b99062e21d4acd8b
Lameck Meshack <rhinolamer@gmail.com>
2023-01-24 00:24:33 +0300

    Updated migration for role

--- database/migrations/2023_01_23_065301_create_roles_table.php
+++ database/migrations/2023_01_23_065301_create_roles_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('roleName');
             $table->string('permission')->nullable();
+            $table->boolean('isAdmin');
             $table->timestamps();
         });
     }
