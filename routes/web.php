db4c6eb2c4a25e314e9ce414f1403a080678c1f2
Lameck Meshack <rhinolamer@gmail.com>
2023-01-23 22:31:09 +0300

    Fixed route error

--- routes/web.php
+++ routes/web.php
@@ -46,8 +46,8 @@ Route::prefix('/app')->middleware('admin')->group(function () {
     //admin
     Route::post('/admin_login', 'AdminController@adminlogin');
     // roles
-    Route::post('/create_role', 'AdminController@addUser');
-    Route::get('/get_roles', 'AdminController@getUser');
+    Route::post('/create_role', 'AdminController@addRole');
+    Route::get('/get_roles', 'AdminController@getRole');
     Route::post('/edit_role', 'AdminCOntroller@editRole');
     Route::post('/delete_role', 'AdminController@delete');
 });
