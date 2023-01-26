b437edeb373c07e864b1bdf52e3fae7e87e3d385
Lameck Meshack <rhinolamer@gmail.com>
2023-01-26 23:26:25 +0300

    Removed extra code in assign componet

--- resources/js/admin/pages/assignRole.vue
+++ resources/js/admin/pages/assignRole.vue
@@ -25,130 +25,46 @@
                         <table class="_table">
                             <!-- TABLE TITLE -->
                             <tr>
-                                <th>ID</th>
-                                <th>Role Type</th>
-                                <th>Created at</th>
-                                <th>Action</th>
+                                <th>Resource Name</th>
+                                <th>Read</th>
+                                <th>Write</th>
+                                <th>Update</th>
+                                <th>Delete</th>
                             </tr>
                             <!-- TABLE TITLE -->
 
                             <!-- ITEMS -->
 
-                            <tr v-for="(role, i) in roles" :key="i">
-                                <template
-                                    style="position: inherit"
-                                    v-if="roles.length"
-                                >
-                                    <td>{{ role.id }}</td>
-                                    <td class="_table_name">
-                                        {{ role.roleName }}
-                                    </td>
-                                    <td>{{ role.created_at }}</td>
-                                    <td>
-                                        <Button
-                                            type="info"
-                                            size="small"
-                                            @click="showEditingModal(role, i)"
-                                            >Edit</Button
-                                        >
-                                        <Button
-                                            type="error"
-                                            size="small"
-                                            @click="showDeletingModal(role, i)"
-                                            >Delete</Button
-                                        >
-                                    </td>
-                                </template>
+                            <tr>
+                                <td>Blog</td>
+                                <td>yes</td>
+                                <td>yes</td>
+                                <td>yes</td>
+                                <td>yes</td>
                             </tr>
                         </table>
                     </div>
                 </div>
-                <!-- role adding modal -->
-                <Modal
-                    v-model="addModal"
-                    title="Add Role"
-                    :mask-closable="false"
-                    :closable="false"
-                >
-                    <Input
-                        v-model="data.roleName"
-                        placeholder="Role Name"
-                        class="modal_input"
-                        @on-keyup.enter="addRole"
-                    ></Input>
-                    <div slot="footer">
-                        <Button type="default" @click="addModal = false"
-                            >Close</Button
-                        >
-                        <Button
-                            type="primary"
-                            @click="addRole"
-                            :disabled="isAdding"
-                            :loading="isAdding"
-                            >{{ isAdding ? "Adding" : "Add Role" }}</Button
-                        >
-                    </div>
-                </Modal>
-                <!-- role editing modal -->
-
-                <Modal
-                    v-model="showEditModal"
-                    title="Edit Role"
-                    :mask-closable="false"
-                    :closable="false"
-                >
-                    <Input
-                        v-model="editData.roleName"
-                        placeholder="Edit a role name"
-                        class="modal_input"
-                        @on-keyup.enter="editRole"
-                    ></Input>
-                    <div slot="footer">
-                        <Button type="default" @click="showEditModal = false"
-                            >Close</Button
-                        >
-                        <Button
-                            type="primary"
-                            @click="editRole"
-                            :disabled="isEditing"
-                            :loading="isEditing"
-                            >{{ isEditing ? "Editing" : "Edit Role" }}</Button
-                        >
-                    </div>
-                </Modal>
-                <!-- role deleting modal -->
-                <DeleteModal />
+                <!--~~~~~~~ TABLE ONE ~~~~~~~~~-->
             </div>
         </div>
     </div>
 </template>
 <script>
-import { mapGetters } from "vuex";
-import DeleteModal from "../components/deleteModal.vue";
 
 export default {
     data() {
         return {
             data: {
                 roleName: "",
-                role_id: "",
+                role_id: null,
             },
-            addModal: false,
-            isAdding: false,
-            isEditing: false,
-            showEditModal: false,
+            resources:[],
+            
             roles: [],
-            editData: {
-                roleName: "",
-            },
-            index: -1,
-            isDeleting: false,
-            deleteItem: {},
-            showDeleteModal: false,
-            deletingIndex: -1,
+            
         };
     },
-    components: { DeleteModal },
     methods: {},
     async created() {
         const res = await this.callApi("get", "app/get_roles");
