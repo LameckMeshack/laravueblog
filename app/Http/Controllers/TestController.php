cfcfefc103d88715581a3e30e6aab0551c026521
Lameck Meshack <rhinolamer@gmail.com>
2023-01-29 23:06:01 +0300

    Removed commented

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,15 +10,4 @@ class TestController extends Controller
     {
         return view('welcome');
     }
-
-    // public function test()
-    // {
-    //     return response()->json(
-    //         [
-    //             'message' => 'Error occurres',
-
-    //         ],
-    //         422
-    //     );
-    // }
 }
