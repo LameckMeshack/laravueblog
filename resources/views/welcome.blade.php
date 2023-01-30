9542a4f6a2f43416b65b5c1424c1ae47a00e1a20
Lameck Meshack <rhinolamer@gmail.com>
2023-01-30 09:53:31 +0300

    Added permissions props

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
 
         @if (Auth::check())
-        <mainapp :user="{{Auth::user()}}"></mainapp>
+    <mainapp :user="{{Auth::user()}}" :permissions="{{Auth::user()->role->permission}}"></mainapp>
         @else
         <mainapp :user="false"></mainapp> 
         @endif
