6cfe3442c44be0c7873b7f16ed73c0e3b342cacd
Lameck Meshack <rhinolamer@gmail.com>
2023-02-02 15:13:13 +0300

    Created jsonToHTML.JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,16 @@ import "./bootstrap";
 import Vue from "vue";
 import ViewUI from "view-design";
 // import Editor from "vue-editor-js";
-import Editor from "vue-editor-js/src/index"; 
+import Editor from "vue-editor-js/src/index";
 import "view-design/dist/styles/iview.css";
 import router from "./router";
 import store from "./store";
 import common from "./common";
+import jsonToHTML from "./jsonToHTML";
 
 Vue.use(ViewUI);
 Vue.mixin(common);
+Vue.mixin(jsonToHTML);
 Vue.use(Editor);
 
 Vue.component("mainapp", require("./components/mainapp.vue").default);
