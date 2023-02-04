770a26f0c63d9af26d138c60f305a9ff474f64c9
Lameck Meshack <rhinolamer@gmail.com>
2023-02-04 21:33:59 +0300

    fixed nix file issues

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").version();
+mix.js("resources/js/app.js", "public/js").vue({ version: 2 });
 
 mix.styles(["public/css/main.css"], "public/css/all.css");
