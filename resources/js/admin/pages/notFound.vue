29dfea84a67f3ad92e0b013e2c899b61c817ba19
Lameck Meshack <rhinolamer@gmail.com>
2023-02-13 20:59:23 +0300

    Refactor  not found

--- resources/js/admin/pages/notFound.vue
+++ resources/js/admin/pages/notFound.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Nof found</h1>
+    <div class="content">
+        <div class="container-fluid">
+            <!--~~~~~~~ TABLE ONE ~~~~~~~~~-->
+            <div
+                class="_1adminOverveiw_table_recent _box_shadow _border_radious _mar_b30 _p20"
+            >
+                <h1>Nof found</h1>
+            </div>
+        </div>
+    </div>
 </template>
+<script></script>
